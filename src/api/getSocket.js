1a6171d381e437f876cfd8b3989c2fe57072864b
cooper09 <cooperatkmba@gmail.com>
2019-03-06 14:18:00 -0500

    cooper s - modifed to use remote VDM server

--- src/api/getSocket.js
+++ src/api/getSocket.js
@@ -1,6 +1,7 @@
 import socketIOClient from 'socket.io-client';
 
-const socket = socketIOClient('http://localhost:8000');
+//const socket = socketIOClient('http://localhost:8000');
+const socket = socketIOClient('http://104.248.110.70:3001')
 
 // Socket events :
 export const socketEvent = {
