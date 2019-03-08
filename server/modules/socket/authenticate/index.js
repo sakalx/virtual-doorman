54cad013d6857d3840b5bdd1db0fded780c8a7ce
sakal <sakals@mail.ua>
2019-03-07 20:49:33 -0500

    feat: authenticate method for socketio-auth
    Signed-off-by: sakal <sakals@mail.ua>

---
+++ server/modules/socket/authenticate/index.js
@@ -0,0 +1,57 @@
+// SQL dummy db users
+const usersSQLdb = [
+  {
+    id: 'some uid',
+    name: 'sakal',
+    password: 'secret',
+  },
+];
+
+// Variables :
+const usersStorage = {
+  'some uid': {
+    id: 'some uid',
+    name: 'some name',
+    token: 'secret token',
+    status: 'offline',
+  },
+};
+
+
+function verifyToken(clientToken, callback) {
+  const verifiedUser = Object.values(usersStorage).find(({token}) => token === clientToken);
+
+  return verifiedUser
+    ? callback(null, verifiedUser)
+    : callback(new Error('Token not found, re-login'));
+}
+
+
+function verifyUserNameAndPassword(userName, password, callback) {
+  const user = usersSQLdb.find(userDB => userDB.name === userName);
+
+  if (!user) return callback(new Error('User not found'));
+
+  if (user.password !== password) return callback(new Error('Incorrect password'));
+
+  const verifiedUser = {
+    id: user.id,
+    name: user.name,
+    token: 'create secret token',
+    status: 'online',
+  };
+
+  usersStorage[user.id] = verifiedUser;
+
+  return callback(null, verifiedUser);
+}
+
+
+function authenticate(socket, {userName, password, clientToken}, callback) {
+
+  return clientToken
+    ? verifyToken(clientToken, callback)
+    : verifyUserNameAndPassword(userName, password, callback);
+}
+
+module.exports = authenticate;
